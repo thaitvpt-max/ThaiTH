48d0469351e2e69109eeac00f6b3edfde9088ebd
thaitvpt-max <thaitvpt@gmail.com>
2026-08-10 08:33:14 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "tien-chuyen-v1";
+const FILES = ["./", "./index.html", "./manifest.json"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
